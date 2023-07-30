8ae5f9a6811870e7ab0150b29dceb6d695f12bab
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-07-30 15:06:47 +0600

    Add files via upload

---
+++ Lab_1/sudipto2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<<"Hello in my University"<<endl;
+  cout<<endl;
+  cout<<"Im a student in it faculity"<<endl;
+  cout<<endl;
+  cout<<"Welcome";
+
+
+    return 0;
+}
