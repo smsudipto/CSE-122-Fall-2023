ef118fddf6642eb73d3f181c12bab983394004f9
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-07-30 15:07:26 +0600

    Add files via upload

---
+++ Lab_1/point.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"Welcome to the wonder fulworld of C++!!!\n";
+   cout<<"Lab1\t";
+   cout<<"\n END\t\PROGRAM\t BYE";
+   cout<<" Welcome in yarmouk university\rHellooo";
+
+
+    return 0;
+}
