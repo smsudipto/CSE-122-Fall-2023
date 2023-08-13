99215d3dc0cd2578eff5e2ecf942f20e40714db3
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-13 15:12:07 +0600

    Add files via upload

---
+++ Lab_03/BIt++.cpp
@@ -0,0 +1,24 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   #include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+   int n,x=0;
+   string s;
+   cin >> n ;
+   for(int i=0;i<n;i++){
+       cin >> s;
+       if(s[1] == '+'){
+          x++;
+       }
+       else {
+           x--;
+       }
+   }
+   cout << x ;
+
+    return 0;
+}
+
