52940ad2724af245cb280dfb36183b44958db991
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-10-05 23:21:52 +0600

    Add files via upload

---
+++ Lab-05/inheritance practic2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class car
+{
+  public:
+      int wheels;
+      int range;
+      int passenger;
+      void display1()
+      {
+          cout<<"Wheels:"<<wheels<<endl;
+          cout<<"Range:"<<range<<endl;
+          cout<<"Passenger:"<<passenger<<endl;
+      }
+};
+class truck:public car
+{
+   public:
+
+      int weight;
+      void display2()
+      {
+         display1();
+      }
+};
+int main()
+{
+    car c1;
+    truck t1;
+    c1.wheels= 4;
+    c1.range= 500;
+    c1.passenger=5;
+    c1.display1();
+    t1.wheels= 12;
+    t1.range= 1200;
+    t1.weight=300;
+    t1.display2();
+
+}
